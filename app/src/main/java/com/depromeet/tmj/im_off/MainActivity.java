fe81458d46a275c1d13fad0d9ca5ff2ac00a1014
Yoo Hyun Sik <dbgustlr92@gmail.com>
2019-04-12 22:11:57 +0900

    설정 화면이동 및 칼퇴기록보기 버튼 추가

--- app/src/main/java/com/depromeet/tmj/im_off/MainActivity.java
+++ app/src/main/java/com/depromeet/tmj/im_off/MainActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.depromeet.tmj.im_off.features.main.TimerFragment;
 import com.depromeet.tmj.im_off.features.main.VerticalViewPager;
 import com.depromeet.tmj.im_off.features.main.ViewPagerAdapter;
 import com.depromeet.tmj.im_off.service.alarm.NotificationAlarmManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimerFragment.ScrollCallback {
 
     private VerticalViewPager viewPager;
 
@@ -19,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
         initNoti();
         initBinding();
         initViewPager();
+    }
+
+    @Override
+    public void onClickStatistics() {
+        viewPager.setCurrentItem(1, true);
     }
 
     private void initBinding() {
--- app/src/main/java/com/depromeet/tmj/im_off/features/main/TimerFragment.java
+++ app/src/main/java/com/depromeet/tmj/im_off/features/main/TimerFragment.java
@@ -1,6 +1,8 @@
 package com.depromeet.tmj.im_off.features.main;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,15 +10,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.depromeet.tmj.im_off.R;
+import com.depromeet.tmj.im_off.SettingActivity;
 import com.depromeet.tmj.im_off.shared.RoundProgressBar;
 
 
 public class TimerFragment extends Fragment {
     private RoundProgressBar roundProgressBar;
+    private ImageButton btnSetting;
+    private TextView tvStatistics;
+    private ScrollCallback scrollCallBack;
 
     public TimerFragment() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(context instanceof ScrollCallback) {
+            scrollCallBack = (ScrollCallback) context;
+        } else {
+            throw new RuntimeException("ScrollCallback is not implemented");
+        }
     }
 
     @Override
@@ -35,15 +53,29 @@ public class TimerFragment extends Fragment {
 
     private void initBinding(View view) {
         roundProgressBar = view.findViewById(R.id.round_progress);
+        btnSetting = view.findViewById(R.id.btn_setting);
+        tvStatistics = view.findViewById(R.id.tv_statistics);
     }
 
     private void initUi() {
         roundProgressBar.setText("09:00");
         roundProgressBar.setProgress(30);
+
+        btnSetting.setOnClickListener(view -> {
+            Intent intent = new Intent(getContext(), SettingActivity.class);
+            startActivity(intent);
+        });
+
+        tvStatistics.setOnClickListener(view -> {
+            scrollCallBack.onClickStatistics();
+        });
     }
 
     public static TimerFragment newInstance() {
         return new TimerFragment();
     }
 
+    public interface ScrollCallback {
+        void onClickStatistics();
+    }
 }
