83b4ef35376e353f8c1ae58015289856db3f5981
Hyunsik-Yoo <dbgustlr92@gmail.com>
2019-04-18 09:33:37 +0900

    #1 출근 전 대기시간일때 남은시간 텍스트가 이상하게 나오는 오류 수정 공유기능 코드 정리

--- app/src/main/java/com/depromeet/tmj/im_off/MainActivity.java
+++ app/src/main/java/com/depromeet/tmj/im_off/MainActivity.java
@@ -1,6 +1,12 @@
 package com.depromeet.tmj.im_off;
 
 import android.content.Intent;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +14,8 @@ import com.depromeet.tmj.im_off.features.main.TimerFragment;
 import com.depromeet.tmj.im_off.features.main.VerticalViewPager;
 import com.depromeet.tmj.im_off.features.main.ViewPagerAdapter;
 import com.depromeet.tmj.im_off.service.alarm.NotificationAlarmManager;
+
+import static com.depromeet.tmj.im_off.features.main.TimerFragment.REQUEST_PERMISSIONS;
 
 public class MainActivity extends AppCompatActivity implements TimerFragment.ScrollCallback {
 
@@ -31,6 +39,16 @@ public class MainActivity extends AppCompatActivity implements TimerFragment.Scr
         viewPager.setCurrentItem(1, true);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS
+                && permissions[0].equals(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setShare();
+        }
+    }
+
     private void initBinding() {
         viewPager = findViewById(R.id.view_pager);
     }
@@ -43,5 +61,13 @@ public class MainActivity extends AppCompatActivity implements TimerFragment.Scr
     private void initNoti() {
         NotificationAlarmManager alarmManager = new NotificationAlarmManager(this);
         alarmManager.registerAll();
+    }
+
+    private void setShare() {
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            if (fragment instanceof TimerFragment) {
+                ((TimerFragment) fragment).setShare();
+            }
+        }
     }
 }
