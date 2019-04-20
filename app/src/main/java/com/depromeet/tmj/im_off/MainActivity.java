692829e66351ed1bee5607d2425fded5b61ad247
Yoo Hyun Sik <dbgustlr92@gmail.com>
2019-04-20 16:13:47 +0900

    MainActivity 사용하지 않는 패키지 제거

--- app/src/main/java/com/depromeet/tmj/im_off/MainActivity.java
+++ app/src/main/java/com/depromeet/tmj/im_off/MainActivity.java
@@ -2,19 +2,8 @@ package com.depromeet.tmj.im_off;
 
 import android.content.Intent;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
